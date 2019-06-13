5a7a26a99e36c657eef86d90aca62404f1129ace
Leo Nistor <leo.nistor@gmail.com>
2019-06-13 17:39:53 +0300

    flutter layout work

--- full_moon_alert/lib/main.dart
+++ full_moon_alert/lib/main.dart
@@ -14,43 +14,65 @@ class FullMoonApp extends StatelessWidget {
   }
 }
 
+const _thinBorder = BorderSide(
+  color: Colors.grey,
+  width: 2.0,
+);
+
 class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
             flex: 6,
             child: Container(
-              child: Center(
-                child: textInfo(
-                    label: 'now', info: 'first quarter', isTitle: true),
+              decoration: BoxDecoration(
+                border: Border(bottom: _thinBorder),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _textInfo(label: 'now', info: 'first quarter', isTitle: true),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
+                    child: FractionallySizedBox(
+                        widthFactor: 0.7,
+                        child: Image.asset('assets/phases/moon_phase08.png')),
+                  ),
+                ],
               ),
             ),
           ),
           Expanded(
             flex: 2,
             child: Container(
-              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _textInfo(label: 'next', info: 'full moon'),
+                  _textInfo(label: 'in', info: '18 days'),
+                  _textInfo(label: 'on', info: 'Wed, April 1st'),
+                ],
+              ),
             ),
           ),
           Expanded(
             flex: 1,
-            child: Container(
-              color: Colors.green,
-            ),
+            child: _buttons(),
           ),
         ],
       ),
     );
   }
 
-  RichText textInfo({String label, String info, bool isTitle = false}) {
+  RichText _textInfo({String label, String info, bool isTitle = false}) {
     return RichText(
       text: TextSpan(
-        style: TextStyle(fontSize: isTitle ? 24.0 : 20.0, color: Colors.white),
+        style: TextStyle(fontSize: isTitle ? 36.0 : 26.0, color: Colors.white),
         children: [
           TextSpan(
             text: label + ' ',
@@ -58,10 +80,59 @@ class StartPage extends StatelessWidget {
           ),
           TextSpan(
             text: info,
-            style: TextStyle(fontWeight: FontWeight.w400),
+            style: TextStyle(fontWeight: FontWeight.w500),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buttons() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Center(
+              child: Text(
+                'About',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                border: Border(
+                  top: _thinBorder,
+                  right: _thinBorder,
+                )),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Center(
+              child: Text(
+                'Set Alert',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.teal,
+                border: Border(
+                  top: _thinBorder,
+                )),
+          ),
+        ),
+      ],
     );
   }
 }
